2dc4320adb76332dc8a7c5ae1bf379c0d522b756
HNM <97424365+hnmpetch@users.noreply.github.com>
2023-09-20 22:19:47 +0700

    Add files via upload

---
+++ hack.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+for i in range(1,5):
+    window = tk.Tk()
+    
+    hi = tk.Label(
+        text="Hack By HNM Studio",
+        foreground="red",
+        background="black",
+        width=20,
+        height=10,
+    )
+    hi.pack()
+    window.mainloop()
